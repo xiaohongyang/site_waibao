e180ac8bee666f258edc46092f2193da5208c154
xiaohongyang <258082291@qq.com>
2017-09-10 23:42:00 +0800

    footer.blade.php

---
+++ resources/views/component/footer.blade.php
@@ -0,0 +1,62 @@
+<div class="footer">
+    <div class="w">
+        <div class="cl">
+            <!--外链 begin-->
+
+            <dl>
+                <dt>关于天博</dt>
+                <dd><a target="_blank" href="/list/17">公司简介</a></dd>
+                <dd><a target="_blank" href="/list/18">资质荣誉</a></dd>
+                <dd><a target="_blank" href="/list/47">发展历程</a></dd>
+                <dd><a target="_blank" href="/list/33">联系我们</a></dd>
+            </dl>
+            <dl>
+                <dt>友情链接</dt>
+                <dd><a target="_blank" href="http://www.gdcic.gov.cn/" target="blank">广东省住房和城乡建设厅</a></dd>
+                <dd><a target="_blank" href="http://szjz.gdcic.net/" target="blank">广东工程监督检测网</a></dd>
+                <dd><a target="_blank" href="http://www.szjs.gov.cn/" target="blank">深圳市住房和建设局</a></dd>
+            </dl>
+
+
+            <dl>
+                <dt>常用链接</dt>
+                <div class="footerid">
+                    <div class="footermain"  style='border-right: 6px solid #666; padding-right: 10px; '>
+                        <dd><a target="_blank" href="http://www.szmqs.gov.cn/" target="blank">深圳市市场和质量监督管理委员会</a></dd>
+                        <dd><a target="_blank" href="http://www.sz.gov.cn/cn/" target="blank">深圳政府在线</a></dd>
+                        <br/>
+                        <br/>
+                    </div>
+                    <div class="footerscorll" >
+                        <div class="foooterbnt">
+                        </div>
+                    </div>
+                </div>
+            </dl>
+            <!--外链 end-->
+            <div class="last f-l" style="margin-left: 10px;">
+                <h4 class="pl"><i class="icon i-phone"></i>0755-28134880</h4>
+                公司地址   深圳市龙华新区大浪街道龙观西路39号龙城工业区综合楼<br/>
+                公司名称   深圳市天博检测技术有限公司<br/>
+                公司电话   0755-28134880<br/>
+                <a href="#"><i class="icon i-wechat"></i><img width="120px" height="120px" src="/tpl/img/wechat.jpg" alt="" class="qrcode"/></a>
+                <a href="#" class='hide'><i class="icon i-weibo"></i></a>
+                <a href="#" class='hide'><i class="icon i-email"></i></a>
+
+            </div>
+        </div>
+        <div class="t1">
+            <a href="/list/46">网站地图</a>&nbsp;|&nbsp;
+            <a href="/list/44">法律声明</a>&nbsp;|&nbsp;
+            <a href="/list/45">检测服务通用条款</a>
+        </div>
+        <div class="t2">
+            Copyright Centre Testing International All rights reserved 粤ICP备17088703号-1
+        </div>
+        <div style="width:215px;margin:0 auto; padding:10px 0;">
+            <a target="_blank" href="http://www.beianbeian.com/gaoji/xiangxi/4C3D33E47AA0A0DCB83A6231F645B760"
+               style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
+                <img src="/tpl/img/police.png" style="float:left;"/><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#fff;">粤ICP备17088703号-1</p></a>
+        </div>
+    </div>
+</div>
