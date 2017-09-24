5695626f48fe6df6b7ca08caaede134fccbb249b
xiaohongyang <258082291@qq.com>
2017-09-25 07:12:29 +0800

    @

---
+++ document/web.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 127.0.0.1
+Source Server Version : 50718
+Source Host           : localhost:3106
+Source Database       : web
+
+Target Server Type    : MYSQL
+Target Server Version : 50718
+File Encoding         : 65001
+
+Date: 2017-09-10 22:48:22
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
--- resources/views/component/common_xhy_js.blade.php
+++ resources/views/component/common_xhy_js.blade.php
@@ -1182,5 +1182,4 @@ MultiRegionPicker.prototype = {
     }
 
 };
-
 </script>
